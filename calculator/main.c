4a16c391aa01fffe6af3b37035d3658710e6e105
Ayelet Drazen <47846150+adrazen@users.noreply.github.com>
2023-02-16 20:02:18 -0800

    Add source files for calculator program

---
+++ calculator/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "operations.h"
+
+
+int main(int argc, char *argv[]){
+	int verbose = 1;
+	int a;
+	int b;
+	char choice;
+	if (strcmp(argv[1],"-q") == 0) {
+		verbose = 0;
+	}
+	if (verbose) {
+		printf("Welcome to the world's most advanced calculator!\n");
+    	printf("After years of research and development, we are proud to welcome you to our new and improved calculator.\n");
+	}
+	if (verbose) {
+		a = atoi(argv[1]);
+		choice = argv[2][0];
+		b = atoi(argv[3]);
+	} else {
+		a = atoi(argv[2]);
+		choice = argv[3][0];
+		b = atoi(argv[4]);
+	}
+	// Evaluate operation based on user's choice.
+    int result; 
+    switch (choice){
+      case '+':
+        result = add(a, b);
+		break;
+      case '-':
+        result = subtract(a, b);
+		break;
+      case '*':
+        result = multiply(a, b);
+		break;
+      case '/':
+        result = divide(a, b);
+		break;
+    }
+	if (verbose) {
+    	printf("The result of your operation is %d.\n", result); 
+	} else {
+		printf("%d\n", result);
+	}
+    return 0;
+  
+}
---
+++ calculator/operations.h
@@ -0,0 +1,4 @@
+int add(int, int);
+int subtract(int, int);
+int multiply(int, int);
+int divide(int, int);
