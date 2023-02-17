fa74583c7292ff25ba5375f2f55da7f957029c2c
Jonathan Kula <jonathan@jdkula.dev>
2023-02-17 00:49:00 -0800

    Add more example tests to 01_example.sh

--- tests/01_example.sh
+++ tests/01_example.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # (The absolute path to the program is provided as the first and only argument.)
-PROGRAM=$1
+CALCULATOR=$1
 
 echo "We've set up a GitHub Actions Workflow that will run all"
 echo "of the shell scripts in this directory as a series of tests."
@@ -9,8 +9,20 @@ echo
 echo "To fail any test, you should use the exit 1 command;"
 echo "To end a test early as a success, you should use the exit 0 command."
 
-echo "Invoke your program with the \$PROGRAM variable; an example is below"
+echo "Invoke your program with the \$CALCULATOR variable; an example is below"
 
 
-# Test 01: Ensure the program runs without error with no arguments.
-$PROGRAM
+# Test 01: Ensure the program runs without error with a simple, valid invocation.
+if ! $CALCULATOR 1 + 1; then  # If the return code of $PROGRAM is non-zero (i.e. error)...
+  exit 1
+fi
+
+# Test 02: Ensure simple case has correct output...
+if [[ $($CALCULATOR 1 + 1) -ne 2 ]]; then  # If the output of the program is not 2...
+  exit 1
+fi
+
+# Test 03: Ensure program errors with an invalid operand
+if $CALCULATOR 3 @ 2; then  # If the return code of $PROGRAM is zero (i.e. success)...
+  exit 1
+fi
