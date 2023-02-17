d9909d5190ccb191ed06fa8083188b0edb20111b
Jonathan Kula <jonathan@jdkula.dev>
2023-02-17 01:22:01 -0800

    Add error logs to the example tests

--- calculator/tests/01_example.sh
+++ calculator/tests/01_example.sh
@@ -14,15 +14,18 @@ echo "Invoke your program with the \$CALCULATOR variable; an example is below"
 
 # Test 01: Ensure the program runs without error with a simple, valid invocation.
 if ! $CALCULATOR 1 + 1; then  # If the return code of $PROGRAM is non-zero (i.e. error)...
+  echo 'ERROR! A valid run of the calculator (1 + 1) failed!'
   exit 1
 fi
 
 # Test 02: Ensure simple case has correct output...
 if [[ $($CALCULATOR 1 + 1) -ne 2 ]]; then  # If the output of the program is not 2...
+  echo 'ERROR! A valid run of the calculator (1 + 1) failed to produce 2 as an output!'
   exit 1
 fi
 
 # Test 03: Ensure program errors with an invalid operand
 if $CALCULATOR 3 @ 2; then  # If the return code of $PROGRAM is zero (i.e. success)...
+  echo 'ERROR! An invalid run of the application (3 @ 2) apparently succeeded?!'
   exit 1
 fi
